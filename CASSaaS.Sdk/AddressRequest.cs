6557feb6093c9c20a561d9fa08466fb48df3a75a
Kevin Becker <kevin.becker@senioroi.com>
2022-06-15 13:12:09 -0400

    Initial commit

---
+++ CASSaaS.Sdk/AddressRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CASSaaS.Api.Models;
+
+public class AddressRequest
+{
+    public int AddressId { get; set; }
+    public string Address1 { get; set; } = default!;
+    public string? Address2 { get; set; }
+    public string City { get; set; } = default!;
+    public string State { get; set; } = default!;
+    public string Zip { get; set; } = default!;
+}
