4ce04ecb6efd5fab5fc64dbcfc4c8843ba267c35
Kevin Becker <kevin.becker@senioroi.com>
2022-06-20 10:37:14 -0400

    Removed ReCASS part of the SDK

--- CASSaaS.Sdk/BatchRequest.cs
+++ CASSaaS.Sdk/BatchRequest.cs
@@ -2,6 +2,5 @@
 
 public class BatchRequest
 {
-    public int RecassOlderThanInDays { get; set; } = 90;
     public List<AddressRequest> Addresses { get; set; } = default!;
 }
--- CASSaaS.Sdk/BatchResponse.cs
+++ CASSaaS.Sdk/BatchResponse.cs
@@ -5,8 +5,7 @@ public class BatchResponse
     public int BatchId { get; set; }
     public int TotalRequested { get; set; }
     public int TotalFound { get; set; }
-    public int TotalFirstCassed { get; set; }
-    public int TotalRecassed { get; set; }
+    public int TotalCassed { get; set; }
     public int TotalInvalid { get; set; }
     public int TotalError { get; set; }
     public List<CassedAddressResponse>? Addresses { get; set; }
