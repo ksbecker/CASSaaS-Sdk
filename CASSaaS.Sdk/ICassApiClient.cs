5fb38febba132a615c2577f48289993619f54b8c
Kevin Becker <kevin.becker@senioroi.com>
2022-07-26 15:27:23 -0400

    Updates

---
+++ CASSaaS.Sdk/ICassApiClient.cs
@@ -0,0 +1,9 @@
+﻿using Refit;
+
+namespace CASSaaS.Sdk;
+
+public interface ICassApiClient
+{
+    [Post("/")]
+    Task<BatchResponse> BatchAsync(BatchRequest request, CancellationToken cancellationToken = default);
+}
---
+++ CASSaaS.Sdk/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Refit;
+
+namespace CASSaaS.Sdk;
+
+public static class ServiceCollectionExtensions
+{
+    public static IHttpClientBuilder AddCassApiClient(this IServiceCollection services, Action<HttpClient> configureClient) =>
+        services.AddRefitClient<ICassApiClient>()
+            .ConfigureHttpClient((httpClient) => configureClient(httpClient));
+}
